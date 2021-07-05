2e64e7121763ef6162c533ea5405266b9d04d8b1
leoho0722 <leo160918@gmail.com>
2021-07-05 23:27:45 +0800

    Finish－SwiftUI Tutorial

---
+++ Creating and Combining Views/Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Leo Ho on 2021/7/5.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
