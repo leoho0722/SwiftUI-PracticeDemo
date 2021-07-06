2b009169057316d71ba83cb08b5135cc700d0305
leoho0722 <leo160918@gmail.com>
2021-07-06 23:27:22 +0800

    Finish－SwiftUI Tutorial

--- Landmarks/Landmarks/Model/ModelData.swift
+++ Landmarks/Landmarks/Model/ModelData.swift
@@ -1,5 +1,5 @@
 //
-//  JSONDecoder.swift
+//  ModelData.swift
 //  Landmarks
 //
 //  Created by Leo Ho on 2021/7/6.
@@ -9,7 +9,17 @@ import Foundation
 import Combine
 
 final class ModelData: ObservableObject {
-   @Published var landmarks: [Landmark] = load("landmarkData.json")
+    @Published var landmarks: [Landmark] = load("landmarkData.json")
+    var hikes: [Hike] = load("hikeData.json")
+    @Published var profile = Profile.default
+    
+    var features: [Landmark] {
+        landmarks.filter {$0.isFeatured}
+    }
+    
+    var categories: [String: [Landmark]] {
+        Dictionary(grouping: landmarks, by: { $0.category.rawValue})
+    }
 }
 
 func load<T: Decodable>(_ filename: String) -> T {
